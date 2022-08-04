11f5c14cd04addde21986b2234b60b5878621448
SevanBadal <sgolnaza@gmail.com>
2022-08-04 17:47:44 -0400

    Add markup string to props

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,18 @@
-import styles from './Card.module.css'
+import DOMPurify from "isomorphic-dompurify";
+import styles from "./Card.module.css";
 
-const Card = () => {
-    function createMarkup() {
-        return {__html: '<h3>I\'m an H3</h3><p>First &middot; Second</p>'};
-      }
-      
-    return (
-        <>
-            <div>
-                <h2>I'm a card with rich text</h2>
-            </div>
-            <div className={styles.card_body}  dangerouslySetInnerHTML={createMarkup()} />
-        </>
-    )
-}
+const Card = ({ richTextString }) => {
+  return (
+    <>
+      <div>
+        <h2>I'm a card with rich text</h2>
+      </div>
+      <div
+        className={styles.card_body}
+        dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(richTextString)}}
+      />
+    </>
+  );
+};
 
 export default Card;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Card from '../components/Card'
 import styles from '../styles/Home.module.css'
+import DOMPurify from 'isomorphic-dompurify';
 
 export default function Home() {
+  function createMarkupString() {
+    return '<h3>I\'m an H3</h3><p>First &middot; Second</p>';
+  }
   return (
     <div className={styles.container}>
       <h1>Rich Text w/styles Demo</h1>
-      <Card />
+      <Card richTextString={createMarkupString()} />
     </div>
   )
 }
